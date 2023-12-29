dbdbff1855a482dd943f130fb4dec3e16839f8f6
SiddharthManjul <manjulsiddharth09@gmail.com>
2023-12-29 23:55:55 +0530

    Techs added

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,58 +4,207 @@ import "./custom.css";
 const About = () => {
   return (
     <>
-      <div className="bg-black mx-4 my-8 rounded-3xl">
-        <div className="py-8 place-items-center flex flex-col mx-auto container text-justify bg-black gradient_intro">
-          <p className="font-josefinSans text-3xl uppercase mb-4">Who I am?!</p>
-          <ul className="font-josefin font-medium text-lg min-[400px]:px-8 list-decimal">
-            <li>
-              Hi! I am a Mechanical Engineer graduated from United College of
-              Engineering & Research in 2023 with grade 6.7 GPA. Also, I'm a
-              Full-Stack Developer who can work in both Web2 & Web3 field. I’m
-              interested in working with & building projects related to Web3.0,
-              Metaverse, Blockchain, Artificial Intelligence, Game Development &
-              IoT. You're probably wondering why & how I switched to this side?!
-            </li>
-            <li>
-              The answer is simple, I developed a keen & unavoidable interest in
-              Blockchain & AI during my last year of Engineering & so I started
-              learning & working on these technologies. I’m currently working on
-              technologies specific to Decentralized field & building projects
-              to solve some crucial problems of Web3 & Crypto World such as
-              Payment System & Proper Regulatory Compilance. I am also
-              interested in learning and working on 3D Animations for commercial
-              Web Apps, Dapps and Games.
-            </li>
-            <li>
-              I’m open for collaboration on Web3 & Blockchain based projects
-              which are scalable in long term.
-            </li>
-            <br />
-            <li>Goals for Q1 2024:</li>
-            <ul className="list-[upper-roman]">
+      <div>
+        <div className="bg-black mx-4 my-8 rounded-3xl">
+          <div className="py-8 place-items-center flex flex-col mx-auto container text-justify bg-black gradient_intro">
+            <p className="font-josefinSans text-3xl uppercase mb-4">
+              Who I am?!
+            </p>
+            <ul className="font-josefin font-medium text-lg min-[400px]:px-8 list-decimal">
               <li>
-                Frontend Goal:- I have good command over React and Next and will
-                improve it further. I will also learn and build some projects
-                using React Native aswell.
+                Hi! I am a Mechanical Engineer graduated from United College of
+                Engineering & Research in 2023 with grade 6.7 GPA. Also, I'm a
+                Full-Stack Developer who can work in both Web2 & Web3 field. I’m
+                interested in working with & building projects related to
+                Web3.0, Metaverse, Blockchain, Artificial Intelligence, Game
+                Development & IoT. You're probably wondering why & how I
+                switched to this side?!
               </li>
               <li>
-                iOS Goal:- By the end of Q1, I will try to become a proficient
-                iOS developer. I will learn Swift and build some projects using
-                Swift and React Native for both of the above Goals.
+                The answer is simple, I developed a keen & unavoidable interest
+                in Blockchain & AI during my last year of Engineering & so I
+                started learning & working on these technologies. I’m currently
+                working on technologies specific to Decentralized field &
+                building projects to solve some crucial problems of Web3 &
+                Crypto World such as Payment System & Proper Regulatory
+                Compilance. I am also interested in learning & working on 3D
+                Animations for commercial Web Apps, Dapps & Games.
               </li>
               <li>
-                Web3 Goal:- Last but not the least, I will also try to become
-                proficient in integrating Frotend with Smart Contracts using
-                different techs. I will build some Full Stack Web3 Projects for
-                this Goal. I have some command over Solidity which I aim to
-                improve further!
+                I’m open for collaboration on Web3 & Blockchain based projects
+                which are scalable in long term.
               </li>
-              <li>
-                I will build a mega project by the end of Q1 using the above
-                tech stack to showcase my improvement and proof-of-work!
-              </li>
+              <br />
+              <li>Goals for Q1 2024:</li>
+              <ul className="list-[upper-roman]">
+                <li>
+                  Frontend Goal:- I have good command over React & Next & will
+                  improve it further. I will also learn & build some projects
+                  using React Native aswell.
+                </li>
+                <li>
+                  iOS Goal:- By the end of Q1, I will try to become a proficient
+                  iOS developer. I will learn Swift & build some projects using
+                  Swift & React Native for both of the above Goals.
+                </li>
+                <li>
+                  Web3 Goal:- Last but not the least, I will also try to become
+                  proficient in integrating Frotend with Smart Contracts using
+                  different techs. I will build some Full Stack Web3 Projects
+                  for this Goal. I have some command over Solidity which I aim
+                  to improve further!
+                </li>
+                <li>
+                  I will build a mega project by the end of Q1 using the above
+                  tech stack to showcase my improvement & proof-of-work!
+                </li>
+              </ul>
             </ul>
-          </ul>
+          </div>
+        </div>
+        <div className="bg-black mx-4 my-8 rounded-3xl">
+          <div className="py-8 place-items-center flex flex-col mx-auto container text-justify bg-black gradient_intro">
+            <p className="font-josefinSans md:text-3xl uppercase mb-4 min-[400px]:text-lg">
+              technologies & languages
+            </p>
+            <div>
+              <p className="capitalize font-josefin text-xl text-center font-semibold mb-2">
+                technologies
+              </p>
+              <div className="">
+                <div className="flex md:flex-row min-[400px]:flex-col bg-grey p-4 rounded-t-3xl gap-x-1">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+                    title="Mongodb"
+                    alt="Mongodb"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
+                    title="Express"
+                    alt="Express"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                    title="React"
+                    alt="React"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
+                    title="Next"
+                    alt="Next"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
+                    title="Node"
+                    alt="Node"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
+                    title="Tailwind"
+                    alt="Tailwind"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+                    title="Github"
+                    alt="Github"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg"
+                    title="Git"
+                    alt="Git"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+                    title="Bash"
+                    alt="Bash"
+                    width="70"
+                    height="70"
+                  />
+                </div>
+                <div className="flex md:flex-row min-[400px]:flex-col bg-grey p-4 rounded-b-3xl gap-x-1">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original-wordmark.svg"
+                    title="Arduino"
+                    alt="Arduino"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/raspberrypi/raspberrypi-original-wordmark.svg"
+                    title="Pi"
+                    alt="Pi"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
+                    title="Npm"
+                    alt="Npm"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original-wordmark.svg"
+                    title="Yarn"
+                    alt="Yarn"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"
+                    title="VSCode"
+                    alt="VSCode"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xcode/xcode-original.svg"
+                    title="Xcode"
+                    alt="Xcode"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg"
+                    title="Jypyter"
+                    alt="Jupyter"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg"
+                    title="Jetbrains"
+                    alt="Jetbrains"
+                    width="70"
+                    height="70"
+                  />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain-wordmark.svg"
+                    title="Ubuntu"
+                    alt="Ubuntu"
+                    width="70"
+                    height="70"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
