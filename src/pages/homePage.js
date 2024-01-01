2b1690b7fc613d8f7040c8d3db6ce02d9eb0df74
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-01-01 23:29:09 +0530

    Responsiveness

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -9,13 +9,13 @@ const HomePage = () => {
       <div className="flex pb-4 bg-black rounded-3xl mx-4 my-8">
         <div className="">
           <img
-            className="max-h-2xl max-w-2xl rounded-r-full pr-20 pt-10 md:hidden lg:block 2xl:max-w-3xl 2xl:max-h-3xl"
+            className="max-h-2xl max-w-2xl rounded-r-full pr-20 pt-10 lg:hidden lg:block 2xl:max-w-3xl 2xl:max-h-3xl"
             src={web2}
             alt="introsidephoto"
           />
         </div>
-        <div className="w-full md:py-8 pr-28 w-full min-[720px]:m-12 min-[720px]:ml-0 min-[720px]:pl-12 md:pl-0 min-[720px]:pr-0 min-[720px]:py-0 2xl:mr-32 2xl:pt-14">
-          <p className="flex font-josefinSans md:text-4xl min-[720px]:text-xl min-[720px]:text-center uppercase font-bold text-left pb-8 md:tracking-wide md:leading-normal gradient_intro 2xl:text-5xl 2xl:leading-normal">
+        <div className="w-full lg:py-8 pr-28 w-full md:m-12 md:ml-0 md:pl-12 lg:pl-0 md:pr-0 md:py-0 2xl:mr-32 2xl:pt-14">
+          <p className="flex font-josefinSans lg:text-4xl md:text-xl md:text-center uppercase font-bold text-left pb-8 lg:tracking-wide lg:leading-normal gradient_intro 2xl:text-5xl 2xl:leading-normal">
             Hello World! I'm Siddharth Manjul, a Frontend Developer with a
             Passion for Blockchain!
           </p>
